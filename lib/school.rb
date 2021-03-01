5941814ae02dc84a2ef646b03e5910abfaac8c5a
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-01 23:12:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+    attr_accessor  :student
+  #  @@roster = {}
+    roster => {grade=>[student]}
+    attr_reader :roster, :grade
+      def initialize(student, grade)
+        @student = student
+        @grade = grade
+        @@roster << self
+      end
+  def add_student(student)
+    roster[grade] << student
+  end
+  def grade(grade)
+    roster[grade] = grade
+
+  end
+  def sort(student)
+    student.sort {|a,b| a <=> b}
+    end
+end
